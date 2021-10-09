27828139d0002537ccee0015b07d1d36127e2385
Lukas Schiesser <lschiesser@uos.de>
2021-10-09 11:19:00 +0200

    included tokenizer in run_preprocessing.py

--- code/preprocessing/run_preprocessing.py
+++ code/preprocessing/run_preprocessing.py
@@ -12,6 +12,7 @@ import argparse, csv, pickle
 import pandas as pd
 from sklearn.pipeline import make_pipeline
 from code.preprocessing.punctuation_remover import PunctuationRemover
+from code.preprocessing.tokenizer import Tokenizer
 
 # setting up CLI
 parser = argparse.ArgumentParser(description = "Various preprocessing steps")
@@ -19,6 +20,7 @@ parser.add_argument("input_file", help = "path to the input csv file")
 parser.add_argument("output_file", help = "path to the output csv file")
 parser.add_argument("-p", "--punctuation", action = "store_true", help = "remove punctuation")
 parser.add_argument("-e", "--export_file", help = "create a pipeline and export to the given location", default = None)
+parser.add_argument("-t", "--tokenize", help="tokenize each sentence", action="store_true")
 args = parser.parse_args()
 
 # load data
@@ -28,6 +30,9 @@ df = pd.read_csv(args.input_file, quoting = csv.QUOTE_NONNUMERIC, lineterminator
 preprocessors = []
 if args.punctuation:
     preprocessors.append(PunctuationRemover())
+
+if args.tokenize:
+    preprocessors.append(Tokenizer())
 
 # call all preprocessing steps
 for preprocessor in preprocessors:
