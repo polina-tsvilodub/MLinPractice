ac5355ef2378c086e01b3f401185401ef61af314
Lukas Schießer <41335130+lschiesser@users.noreply.github.com>
2021-10-26 09:41:03 +0200

    Update code/feature_extraction.sh
    Co-authored-by: polina-tsvilodub <42241414+polina-tsvilodub@users.noreply.github.com>

--- code/feature_extraction.sh
+++ code/feature_extraction.sh
@@ -11,4 +11,4 @@ python -m code.feature_extraction.extract_features data/preprocessing/split/trai
 echo "  validation set"
 python -m code.feature_extraction.extract_features data/preprocessing/split/validation.csv data/feature_extraction/validation.pickle -i data/feature_extraction/pipeline.pickle --char_length --embedding -dt
 echo "  test set"
-python -m code.feature_extraction.extract_features data/preprocessing/split/test.csv data/feature_extraction/test.pickle -i data/feature_extraction/pipeline.pickle --char_length --embedding
+python -m code.feature_extraction.extract_features data/preprocessing/split/test.csv data/feature_extraction/test.pickle -i data/feature_extraction/pipeline.pickle --char_length --embedding -dt
