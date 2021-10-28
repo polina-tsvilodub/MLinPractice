964c2c057ffaf4af8f94b0225d33d6713595ada3
polina-tsvilodub <42241414+polina-tsvilodub@users.noreply.github.com>
2021-10-28 22:48:21 +0200

    Remove CV and gridsearch, bump SVC to LinearSVC

--- code/classification/run_classifier.py
+++ code/classification/run_classifier.py
@@ -10,7 +10,7 @@ Created on Wed Sep 29 14:23:48 2021
 
 import argparse, pickle
 from sklearn.dummy import DummyClassifier
-from sklearn.svm import SVC
+from sklearn.svm import LinearSVC
 from code.evaluation.evaluation_metrics import EvaluationMetrics 
 from code.util import EVAL_RESULTS_PATH
 from sklearn.model_selection import GridSearchCV
@@ -52,27 +52,28 @@ else:   # manually set up a classifier
     if args.svm_classifier:        
         print("    production SVM classifier")
         # SVM classifier with default parameters
-        classifier_svm = SVC()
+        classifier_svm = LinearSVC()
         # cross-validation with grid search
         # dict of hyperparameters to optimize
-        parameters = {"kernel": ("linear", "rbf"), 
-                      "gamma": ("scale", "auto"),
-                      "C": [1, 5]}
-        svm_clf = GridSearchCV(
-            estimator=classifier_svm, 
-            param_grid=parameters, 
-            refit=True, 
-            cv=args.svm_k 
-            )
+#         parameters = {"kernel": ("linear", "rbf"), 
+#                       "gamma": ("scale", "auto"),
+#                       "C": [1, 5]}
+#         svm_clf = GridSearchCV(
+#             estimator=classifier_svm, 
+#             param_grid=parameters, 
+#             refit=True, 
+#             cv=args.svm_k 
+#             )
         # call grid search and CV
-        svm_clf.fit(data["features"], data["labels"])
-      
+#         svm_clf.fit(data["features"], data["labels"])
+        # fit classifier
+        classifier_svm.fit(data["features"], data["labels"])
         # get best classifier
-        svm_best = svm_clf.best_estimator_
-        print('     Best SVM classifier parameters: ', svm_best)
+#         svm_best = svm_clf.best_estimator_
+#         print('     Best SVM classifier parameters: ', svm_best)
         
         # now classify the given data with best classifier instance
-        prediction_svm = svm_best.predict(data["features"])
+        prediction_svm = classifier_svm.predict(data["features"])
 
         # set up evaluator class instance for crossvalidation results of the SVM
         evaluator_cv = EvaluationMetrics(y_true=data["labels"], y_pred=prediction_svm) 
@@ -86,7 +87,7 @@ else:   # manually set up a classifier
         # export the trained classifier if the user wants us to do so
         if args.export_file is not None:
             with open(args.export_file, 'wb') as f_out:
-                pickle.dump(svm_best, f_out)
+                pickle.dump(classifier_svm, f_out)
         
 # classify data with the given train data with baseline dummy classifier
 prediction_dummy = classifier.predict(data["features"])     
